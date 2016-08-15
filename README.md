4625b1ce953d6b3f619937d064feb1e831f977ca
Steven Galante <smgalante85@gmail.com>
2016-08-15 10:19:33 -0400

    small edits

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# burger
+# BurgerApp
+
+App Summary
+
+Burgers! is a fun app that lets a user input the name of a burger they want to eat.
+
+Once submitted, the burger is displayed in text on the left side of the page where it's waiting to be devoured.
+
+Each burger in the waiting area also has a Devour it! button. Once clicked, the burger will move to the right side of the page.
+
+Every burger entered is stored in the database.
---
+++ config/orm.js
@@ -0,0 +1,43 @@
+var connection = require('../config/connection.js');
+
+var orm = {
+  all: function(table, cb) {
+    var queryString = 'SELECT * FROM ' + table + ';';
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  },
+  create: function(table, cols, vals, cb) {
+    var queryString = 'INSERT INTO ' + table + ' (';
+    queryString += cols.toString();
+    queryString += ') VALUES (?)';
+    console.log(queryString);
+    connection.query(queryString, [vals], function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  },
+  update: function(table, objColVals, condition, cb) {
+    var queryString = 'UPDATE ' + table + ' SET ' + objColVals + ' WHERE ' + condition;
+    console.log(queryString);
+    console.log(objColVals);
+    connection.query(queryString, function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  },
+  delete: function(table, condition, cb) {
+    var queryString = 'DELETE FROM ' + table;
+    queryString += ' WHERE ';
+    queryString += condition;
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
